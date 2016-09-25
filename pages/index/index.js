0b04002510e80f41978ac93a61033842fa99a2f6
Walker <hiwangchi@gmail.com>
2016-09-25 11:13:32 +0800

    simple profile

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,8 +3,11 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    userInfo: {
+      nickName: 'Walker',
+      avatarUrl: '../../images/photo.jpg'
+    },
+    motto: 'Front-end Developer'
   },
   //事件处理函数
   bindViewTap: function() {
@@ -13,14 +16,15 @@ Page({
     })
   },
   onLoad: function () {
-    console.log('onLoad')
+    // console.log('onLoad')
     var that = this
-    //调用应用实例的方法获取全局数据
+  	//调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
       //更新数据
       that.setData({
         userInfo:userInfo
       })
+      that.update()
     })
   }
 })
